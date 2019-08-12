263a2ab21bb5d3810d0f76dad347fba1316482cf
Yura Sachenko <yurasachenko@MacBook-Pro-Yura.local>
2019-08-12 20:04:27 +0300

    scrolled header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,3 @@
-function openModal(e) {
-    iOS && iOS11 && (scrolledPositionBeforeOpenModal = $(document).scrollTop()), $("body, .page-header").css("padding-right", scrollbarWidth), $("body, html").addClass("modal-opened"), "header-menu" == $(e).attr("id") && $(window).width() < 768 ? $(e).addClass("active") : $(e).fadeIn(300)
-}
-
-function closeModals() {
-    scrollbarWidth > 0 ? $(".popup-block:not(:hidden)").fadeOut(200, function() {
-        $("body, .page-header").css("padding-right", 0), $("body, html").removeClass("modal-opened"), iOS && iOS11 && $("html, body").scrollTop(scrolledPositionBeforeOpenModal)
-    }) : ($("body, .page-header").css("padding-right", 0), $("body, html").removeClass("modal-opened"), iOS && iOS11 && $("html, body").scrollTop(scrolledPositionBeforeOpenModal), $(".popup-block:not(:hidden)").fadeOut(200)), $("#header-menu").removeClass("active")
-}
-
-function showThanksModal() {
-    $(".popup-block:not(:hidden)").fadeOut(), $("#thanks-popup").fadeIn(300)
-}
-var ua = navigator.userAgent,
-    iOS = /iPad|iPhone|iPod/.test(ua),
-    iOS11_0 = /OS 11_0_?(\d+)?/.test(ua),
-    iOS11_1 = /OS 11_1_?(\d+)?/.test(ua),
-    iOS11_2 = /OS 11_2_?(\d+)?/.test(ua),
-    iOS11 = iOS11_0 || iOS11_1 || iOS11_2,
-    scrolledPositionBeforeOpenModal;
-iOS && iOS11 && $("body").addClass("iosBugFixCaret"), /Android/.test(navigator.appVersion) && window.addEventListener("resize", function() {
-    "INPUT" != document.activeElement.tagName && "TEXTAREA" != document.activeElement.tagName || document.activeElement.scrollIntoView()
-});
-
-var block = $('<div>').css({'height':'50px','width':'50px'}),
-    indicator = $('<div>').css({'height':'200px'}),
-    scrollbarWidth = 0;
-
-$('body').append(block.append(indicator));
-var w1 = $('div', block).innerWidth();    
-block.css('overflow-y', 'scroll');
-var w2 = $('div', block).innerWidth();
-$(block).remove();
-scrollbarWidth = (w1 - w2);
-
-
 $(document).ready(function () {
  if( $(".swiper-container").length ) {
 	  var mySwiper = new Swiper ('.swiper-container', {
@@ -55,8 +19,13 @@ $(document).ready(function () {
  }
 });
 
-$(window).scroll(function() {
-    var e = 200,
-        t = 300;
-    $(this).scrollTop() > e && !$("body").hasClass("scrolled") ? $("body").addClass("scrolled") : $(this).scrollTop() <= e && $("body").hasClass("scrolled") && $("body").removeClass("scrolled"), $(this).scrollTop() > t && !$("body").hasClass("menu-fixed") ? $("body").addClass("menu-fixed") : $(this).scrollTop() <= t && $("body").hasClass("menu-fixed") && $("body").removeClass("menu-fixed")
+$('.page-header').on('scroll load', function() {
+ var headerFixedHeight = 200;
+ 
+ if ($(this).scrollTop() > headerFixedHeight && !($('body').hasClass("scrolled")) ) {
+  $('body').addClass("scrolled");
+ } else if($(this).scrollTop() <= headerFixedHeight && $('body').hasClass("scrolled")) {
+  $('body').removeClass("scrolled");
+ }
+ 
 });
